f713349997025c2656795ea29cae263f90403d26
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-05-31 01:26:32 +0300

    feat: added LoginForm

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import {LoginForm} from "@/features/login/ui/LoginForm";
+
+export default function LoginPage() {
+    return (
+        <main className='w-screen h-screen flex items-center justify-center'>
+            <LoginForm />
+        </main>
+    )
+}
